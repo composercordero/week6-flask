82171f8b368253653d2c9c3c8aadbb2d34a74234
composercordero <composercordero@gmail.com>
2023-08-14 23:42:21 -0500

    portfolio flask

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/fav')
+def favorite():
+    return render_template('favorite.html')
